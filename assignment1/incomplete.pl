b61c69b2633f7b3b795cedd961dd4ad6e996cc88
darts <darts@tcd.ie>
2020-03-09 13:52:46 +0000

    Done

--- assignment1/incomplete.pl
+++ assignment1/incomplete.pl
@@ -40,27 +40,45 @@ initKB(File) :-
 %-----------------------------------------------------------
 
 %user input
-astar(Node,Path,Cost) :- kb(KB), astar(Node,Path,Cost,KB).
+astar(Node, Path, Cost) :-
+    kb(KB),
+    astar([[Node, [], 0]], Path, Cost, KB).
 
-astar(Node,Path,Cost,KB) :- 
-
-
-search([Node|_]) :-
+%final node reached
+astar([[Node, Path, Cost]|_], [Node, Path], Cost, _) :-
     goal(Node).
 
-search([Node|More]) :-
-    findall(X, arc(Node, X), Children),
-    add-to-frontier(Children, More, New),
-    search(New).
+%some still unexplored
+astar([[Node, NP, NC]|Rest], Path, Cost, KB) :-
+    findall([P, [Node|NP], Sum],
+            ( arc(Node, P, Q, KB),
+              Sum is Q+NC
+            ),
+            Children),
+    add-to-frontier(Children, Rest, New),
+    astar(New, Path, Cost, KB).
 
+add-to-frontier(Children, More, New) :-
+    append(Children, More, Tmp),
+    sortByMin(Tmp, New).
 
+%need to include heuristic function in sort
+sortByMin([OldH|OldT], New) :-
+    sortByMinRec(OldH, [], OldT, New).
+% sortByMin(Old, New) :- sort(Old, New).
 
-add-to-frontier(Children, More, New) :- append(Children, More, tmp), sortByMin(tmp, New).
-
-
+%list is empty, all sorted
+sortByMinRec(H, Rem, [], [H|Rem]).
+%list is not empty, sort
+sortByMinRec(H, Swp, [RemH|RemT], New) :-
+    (   lessThan(H, RemH),
+        !,
+        sortByMinRec(H, [RemH|Swp], RemT, New)
+    ;   sortByMinRec(RemH, [H|Swp], RemT, New)
+    ).
 
 %-----------------------------------------------------------
-less-than([[Node1|_], Cost1], [[Node2|_], Cost2]) :-
+lessThan([[Node1|_], Cost1], [[Node2|_], Cost2]) :-
     heuristic(Node1, Hvalue1),
     heuristic(Node2, Hvalue2),
     F1 is Cost1+Hvalue1,
